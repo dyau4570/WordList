c9594094b60e7f20387120782b853d9fe5963356
BT_1N3_10 <BT_1N3_10>
2018-12-20 11:27:48 -0500

    Initial commit

---
+++ src/DarrenYau/Runner.java
@@ -0,0 +1,19 @@
+package DarrenYau;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Runner {
+
+    public static void main(String[] args) {
+	    Arraylist animalArr = new ArrayList(Arrays.asList("cat", "mouse", "frog", "dog" ,"dog"));
+
+	    WordList animals = new WordList(animalArr);
+
+        System.out.println(animals.numWordsOfLength(4));
+        System.out.println(animals.numWordsOfLength(3));
+        System.out.println(animals.numWordsOfLength(2));
+
+        animals.removeWordsOfLength(4);
+    }
+}
---
+++ src/DarrenYau/animals.java
@@ -0,0 +1,4 @@
+package DarrenYau;
+
+public class animals {
+}
